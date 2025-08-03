db0d83feb6c965356323347dddeb4083a52e64b9
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-03 17:23:30 +0300

    jdk-amd64:17.0.11b

--- debian-amd64/bullseye-20250721-slim/jdk/17.0.11/test.sh
+++ debian-amd64/bullseye-20250721-slim/jdk/17.0.11/test.sh
@@ -5,7 +5,7 @@ PLATFORM="linux/${ARCH}"
 HOST='docker.io'
 NAMESPACE='kepocnhh'
 REPOSITORY="jdk-${ARCH}"
-TAG='17.0.11'
+TAG='17.0.11b'
 IMAGE_NAME="${HOST}/${NAMESPACE}/${REPOSITORY}:${TAG}"
 
 docker build --no-cache -f Dockerfile \
