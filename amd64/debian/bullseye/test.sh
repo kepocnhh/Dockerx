c79c427c94258871121bb22eb0fade062e73aff2
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-09 16:58:08 +0300

    debian-amd64:8d-bullseye

--- amd64/debian/bullseye/test.sh
+++ amd64/debian/bullseye/test.sh
@@ -5,11 +5,10 @@ PLATFORM="linux/${ARCH}"
 HOST='docker.io'
 NAMESPACE='kepocnhh'
 REPOSITORY="debian-${ARCH}"
-TAG='8c-bullseye'
+TAG='8d-bullseye'
 IMAGE_NAME="${HOST}/${NAMESPACE}/${REPOSITORY}:${TAG}"
 
-docker build --no-cache -f Dockerfile \
- --platform="${PLATFORM}" -t "${IMAGE_NAME}" .
+docker build --no-cache -f "${ARCH}/debian/bullseye/Dockerfile" --platform="${PLATFORM}" -t "${IMAGE_NAME}" .
 
 if test $? -ne 0; then
  echo "Docker build error!"; exit 21; fi
@@ -25,10 +24,13 @@ docker run --platform="${PLATFORM}" \
 if test $? -ne 0; then
  echo 'Run error!'; exit 1; fi
 
+docker cp "${ARCH}/debian/bullseye/key.pgp" "${CONTAINER_NAME}:/tmp/key.pgp"
+if test $? -ne 0; then echo 'Copy error!'; exit 1; fi
+
 for it in \
+ 'cat /etc/apt/sources.list' \
  'curl --version' \
  'openssl version' \
- 'gpg --version' \
  'zip --version' \
  'yq --version' \
  'echo "foo: bar" | yq -erM .foo' \
@@ -40,6 +42,21 @@ for it in \
  '/usr/local/bin/bash --version'; do
  docker exec "${CONTAINER_NAME}" /usr/local/bin/bash -c "$it"
  if test $? -ne 0; then echo 'Exec error!'; exit 1; fi
+done
+
+for it in \
+ 'gpg --version' \
+ 'cat /usr/local/bin/gpgloopback.sh' \
+ '/usr/local/bin/gpgloopback.sh --version; echo $?' \
+ 'GPG_PASSWORD=foobarbaz /usr/local/bin/gpgloopback.sh --version' \
+ 'gpg --batch --import /tmp/key.pgp' \
+ 'gpg --list-keys' \
+ 'gpg --list-secret-keys --keyid-format=long' \
+ 'GPG_PASSWORD=foobarbaz /usr/local/bin/gpgloopback.sh --output /tmp/key.pgp.sig --detach-sig /tmp/key.pgp; echo $?' \
+ 'GPG_PASSWORD=qwer1234 /usr/local/bin/gpgloopback.sh --output /tmp/key.pgp.sig --detach-sig /tmp/key.pgp' \
+ 'gpg --verify /tmp/key.pgp.sig /tmp/key.pgp'; do
+ docker exec "${CONTAINER_NAME}" /usr/local/bin/bash -c "$it"
+ if test $? -ne 0; then echo 'GPG error!'; exit 1; fi
 done
 
 docker stop "${CONTAINER_NAME}"
