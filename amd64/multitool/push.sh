9c6a33ba1b8f44a1281a54acac63864be1448660
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-12 17:56:31 +0300

    multitool-0.6.2-amd64:6d

--- amd64/multitool/push.sh
+++ amd64/multitool/push.sh
@@ -6,9 +6,9 @@ HOST='docker.io'
 NAMESPACE='kepocnhh'
 
 ISSUER='multitool'
-ISSUER_VERSION='0.6.1'
+ISSUER_VERSION='0.6.2'
 REPOSITORY="${ISSUER}-${ISSUER_VERSION}-${ARCH}"
-IMAGE_VERSION=1
+IMAGE_VERSION=6
 IMAGE_FLAVOR='d'
 IMAGE_TAG="${IMAGE_VERSION}${IMAGE_FLAVOR}"
 IMAGE_NAME="${HOST}/${NAMESPACE}/${REPOSITORY}:${IMAGE_TAG}"
@@ -27,8 +27,10 @@ docker rm -f "${CONTAINER_NAME}"
 docker run --platform="${PLATFORM}" \
  -e REPOSITORY_OWNER='kepocnhh' \
  -e REPOSITORY_NAME='Useless.Java.Lib' \
- -e SOURCE_COMMIT='12dbb64184ab4206b753e310e9caa0022fff6d16' \
+ -e SOURCE_COMMIT='5dd636be4b8e236f0ba5df28bcd934b923b718d3' \
  -e TARGET_BRANCH='master' \
+ -e GPG_PASSWORD='qwer1234' \
+ -e GPG_KEY_ID='2AC43613F5502EB3C490D2C62CFF9BD0725E548B' \
  -id --name "${CONTAINER_NAME}" "${IMAGE_NAME}"
 
 if test $? -ne 0; then echo 'Run error!'; exit 1; fi
@@ -40,6 +42,9 @@ if test $? -ne 0; then echo 'Flavor error!'; exit 1; fi
 docker exec "${CONTAINER_NAME}" /usr/local/bin/bash -c \
  "test \"\${MULTITOOL_VERSION}\" == \"${ISSUER_VERSION}\""
 if test $? -ne 0; then echo 'Version error!'; exit 1; fi
+
+docker cp "${ARCH}/${ISSUER}/key.pgp" "${CONTAINER_NAME}:/tmp/key.pgp"
+if test $? -ne 0; then echo 'Copy error!'; exit 1; fi
 
 for it in \
  'git init' \
@@ -57,6 +62,11 @@ for it in \
  'java --version' \
  'gradle --version' \
  'mvn --version' \
+ 'gpg --version' \
+ 'gpg --batch --import /tmp/key.pgp' \
+ 'gpg --list-keys && gpg --list-secret-keys --keyid-format=long' \
+ 'git config gpg.program "/usr/local/bin/gpgloopback.sh"' \
+ 'git config user.signingkey "${GPG_KEY_ID}"' \
  '$mt/vcs/merge.sh' \
  '$mt/java/lib/unstable/assemble.sh' \
  '$mt/vcs/commit.sh "msg" "tag"' \
